e3881c6ddc7ccb4dbc704d3379826cbdd7ad942c
Rishikesh Reddy Pullaiahgari <prishikesh.06@gmai.com>
2024-02-08 13:25:21 -0500

    Code after few  changes

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -23,34 +23,42 @@ public static class SeedData
                 new Movie
                 {
                     Title = "Bahubali",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                    Genre = "Family",
-                    Rating = "R",
-                    Price = 7.99M
+                    ReleaseDate = DateTime.Parse("2015-12-15"),
+                    Genre = "Ancient",
+                    Rating = "9.0",
+                    Price = 12M
                 },
                 new Movie
                 {
                     Title = "Naruto",
-                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    ReleaseDate = DateTime.Parse("2015-06-22"),
                     Genre = "Anime",
-                    Rating = "R",
-                    Price = 8.99M
+                    Rating = "9.5",
+                    Price = 10M
                 },
                 new Movie
                 {
                     Title = "One Piece",
-                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    ReleaseDate = DateTime.Parse("2003-03-12"),
                     Genre = "Anime",
-                    Rating = "R",
-                    Price = 9.99M
+                    Rating = "9.8",
+                    Price = 17M
                 },
                 new Movie
                 {
                     Title = "Annabelle",
-                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    ReleaseDate = DateTime.Parse("2013-03-04"),
                     Genre = "Horror",
-                    Rating = "R",
-                    Price = 3.99M
+                    Rating = "9",
+                    Price = 8.79M
+                },
+                new Movie
+                {
+                    Title = "Mirchi",
+                    ReleaseDate = DateTime.Parse("2015-06-17"),
+                    Genre = "Family Entertainer",
+                    Rating = "8.5",
+                    Price = 8.99M
                 }
             );
             context.SaveChanges();
